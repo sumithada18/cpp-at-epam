e6e995c40a49219cb26976ebc40d853d94ca8b01
sumithada18 <sumit_hada@epam.com>
2025-02-11 17:43:08 +0530

    Update pointers1.cpp

--- pointers/pointers1.cpp
+++ pointers/pointers1.cpp
@@ -3,10 +3,27 @@ using namespace std;
 
 int main()
 {
+	/*
+ pointer -> a varible that stores the memory address as it's value.
+ example:-
+ 	string name = "sumit"; //a name variable of type string.
+ 	string* ptr = &name; //a pointer variable with the name ptr.
+
+   	cout << name << endl; //outputs value of name(sumit).
+    	cout << &name << endl; //outputs memory address of name.
+     	cout << ptr << endl; //m/m address of name is printed.
+	*/
+
+	/*
+ Note: Both are the same.
+ int *ptr;
+ int* ptr
+	*/
+	
 	//derefrence pointer(*)->value at address
 	int a=10;
 	int *ptr=&a;
-	//cout<<ptr<<" "<<&a<<endl;
+	//cout << ptr << " " << &a << endl; //&a means value at address started 
 
 	//double pointer
 	int **parPtr = &ptr;
