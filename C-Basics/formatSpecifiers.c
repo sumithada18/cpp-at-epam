725c9fb131b62d27a2dd33c85c111d322fa9838a
sumithada18 <sumit_hada@epam.com>
2025-02-24 19:21:18 +0530

    Add files via upload

---
+++ C-Basics/formatSpecifiers.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main()
+{
+    //int num = 32;
+    //printf("%d", num);
+    
+    /*
+    double num1 = 3.141592653589793;
+    printf("%lf \n", num1); 
+    
+    float num2 = 3.141592653589793;
+    printf("%f", num2);
+    */
+    
+    /*
+    double num1 = 3.141592653589793;
+    printf("%.15lf \n", num1); 
+    
+    float num2 = 3.141592653589793;
+    printf("%.15f", num2);
+    
+    float (32-bit) stores only ~6-7 digits accurately, and extra digits may be rounded or incorrect.
+    double (64-bit) stores 15-16 digits accurately, making it more precise.
+    Even if you print more digits, float cannot store extra precision, while double can.
+    */
+    
+    //long long num = 1098765;
+    //printf("%lld", num);
+    
+    //-----Address format specifier----
+    //int a = 10;
+    //printf("%p", &a);
+    
+    //-----String format specifier-----
+    //char a[] = {"Hello world"};
+    //printf("%s", a);
+    
+
+    return 0;
+}
