8d2b8dde9ad3c42c532e80181a4e84c583cb3e7c
sumithada18 <sumit_hada@epam.com>
2025-02-25 09:16:21 +0530

    Update formatSpecifiers.c

--- C-Basics/formatSpecifiers.c
+++ C-Basics/formatSpecifiers.c
@@ -35,7 +35,30 @@ int main()
     //-----String format specifier-----
     //char a[] = {"Hello world"};
     //printf("%s", a);
+
+     int num = 255;
+    // %x prints an integer in lowercase hexadecimal (base 16).
+    printf("%x \n", num);
+    // %X prints an integer in uppercase hexadecimal (base 16).
+    printf("%X \n", num);
+    //Hexadecimal uses digits 0-9 and letters A-F (or a-f in lowercase).
     
+    
+    
+    float num2 = 12345.6789;
+    // %e prints a floating-point number in scientific notation with a lowercase 'e'.
+    printf("Lowercase scientific notation: %e \n", num2);
+    // %E prints the same value but with an uppercase 'E'.
+    printf("Lowercase scientific notation: %E \n", num2);
+    //12345.6789 -> 1.23456789 x 10^4 or 1.234568E+04 
+    
+    
+    char* ptr = "sumit";
+    printf("%10.2s", ptr);
+    // 10 → Reserves 10-character width (right-aligned by default).
+    // .2 → Prints only first 2 characters of the string.
+    // If the string is shorter than 10, spaces are added for alignment.
+
 
     return 0;
 }
