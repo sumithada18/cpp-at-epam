0d9f72182760c0651551bd43c7606b375a11658a
sumithada18 <sumit_hada@epam.com>
2025-04-17 10:27:52 +0530

    Ordered map and multimap

---
+++ map/multiMap.cpp
@@ -0,0 +1,65 @@
+﻿//#include <iostream>
+//#include <string> 
+//#include <map>
+//
+//int main() {
+//	// std::multimap is an associative container that contains a sorted list of key-value pairs, while permitting multiple entries with the same key.
+//	std::multimap<int, std::string>mm;
+//
+//	// insertion
+//	mm.insert({ 1,"sky" });
+//	mm.insert({ 2,"book" });
+//	mm.insert({ 1,"door" }); // duplicates are allowed
+//
+//	// printing the multimap using iterators
+//	std::multimap<int, std::string>::iterator itr = mm.begin();
+//	while (itr!=mm.end()) {
+//		std::cout << "key: " << itr->first << " value: " << itr->second << "\n";
+//		itr++;
+//	}
+//
+//	/*
+//	Q: How do iterators work in map and multimap if they're implemented using trees, not arrays?
+//	- Iterators follow in-order traversal internally (Left → Root → Right).
+//	- ++it moves to the next node in sorted key order.
+//	- This makes tree-based containers behave like sorted sequences during iteration.
+//	*/
+//
+//	// emmplace : constrcuts in-place
+//	mm.emplace(2, "pin");
+//	mm.emplace(4, "tin");
+//
+//	// count 
+//	std::cout << "count of key:2 is = " << mm.count(2) << "\n";
+//
+//	// find
+//	auto it = mm.find(1); // returns iterator to the first key found
+//	if (it != mm.end()) {
+//		std::cout << "key found! with key: " << it->first << " value: " << it->second << "\n";
+//	}
+//	else {
+//		std::cout << "key not found!" << "\n";
+//	}
+//
+//	// erase
+//	mm.erase(4); // passing key O(log n)
+//	auto e = mm.begin();
+//	mm.erase(e); // passing iterator O(1)
+//	
+//	// important feature - equal_range()
+//	std::multimap<int, char>mmic = { {2,'a'},{3,'a'},{2,'c'},{4,'c'}};
+//	auto i = mmic.equal_range(2);
+//
+//	/*
+//	equal_range(key) returns a pair of iterators:
+//	first → points to the first occurrence of key
+//	second → points to the iterator just after the last occurrence of key
+//	*/
+//
+//	for (auto itr = i.first; itr != i.second;++itr) {
+//		std::cout << "key: " << itr->first << " val: " << itr->second << "\n";
+//	}
+//	// helpful in getting all the values of a certain key in multimap!
+//
+//	return 0;
+//}
---
+++ map/stdMap.cpp
@@ -0,0 +1,150 @@
+﻿#include<iostream>
+#include <string>
+#include<map>
+
+//struct Student {
+//	std::string _name;
+//	int _id;
+//	Student(int id, std::string name) : _id(id), _name(name) {
+//		std::cout << "student created with name: " << _name << "\n";
+//	}
+//	Student(const Student& obj) {
+//		std::cout << "copy called!" << "\n";
+//	}
+//	Student() = default; // default constructor added
+//	Student& operator=(const Student& obj) {
+//		std::cout << "copy assignment \n";
+//		return *this;
+//	}
+//};
+
+
+
+
+int main() {
+	
+	std::map<int, int>mpp; // empty map
+	std::map<int, char> mpp2= { {1,'a'},{2,'b'},{3,'c'}}; // initializer list
+	// printing map:
+	/*for (auto& i : mpp2) {
+		std::cout << "key: " << i.first << " val: " << i.second << "\n";
+	}*/
+
+	std::map<int, std::string>mpp3{{2, "sf"}, { 1,"a" }, { 4,"aegf" }, { 5,"egefq" }}; // uniform initialization(c++11)
+	// printing map:
+	/*for (auto& i : mpp3) {
+		std::cout << "key: " << i.first << " val: " << i.second << "\n";
+	}*/
+
+	std::map<int, char>mpp4(mpp2); // copy cons..
+
+	std::map<int, std::string>mpp5(std::move(mpp3)); // move cons...
+
+	std::map<int, char>mpp6 = mpp4; // copy assignment
+
+	std::map<int, std::string>mpp7 = std::move(mpp5); // move assignment 
+
+	// insertion in map
+	std::map<char, int>mci= {{'c',4}, {'a',1}, {'f',8}};
+	mci.insert({'t',9});
+	// or
+	mci['a'] = 23; // if key does not exist - it'll create the new entry
+	mci['c'] = 12; // if key alredy exists - it updates it
+	//for (auto& i : mci) {
+	//	std::cout << "key: " << i.first << " val: " << i.second << "\n";
+	//}
+
+	// emplace - constructs in-place
+	//mci.emplace('w',18);  // emplace constrcuts the key-value pair directly
+	////mci.insert('r',45)  // gives error!!! - insert requires that the pair must be already constructed
+	//mci.insert({'r',45}); 
+	//// or
+	//mci.insert(std::make_pair('r', 45));
+
+
+	/*
+	std::map<int, Student>studMap;
+	studMap.insert({1, Student(123, "sumit")});
+	studMap.insert({2, Student(301, "sparsh")});
+
+	// inserting using emplace
+	studMap.emplace(3, Student(239, "priyanshu")); // this works but - temporary copy is created
+	// studMap.emplace(4, 420, "kratik"); - incorrect syntax
+
+	// better approach:
+	studMap.emplace(
+		std::piecewise_construct,
+		std::forward_as_tuple(4),
+		std::forward_as_tuple(420, "kratik")
+	);
+	*/
+	/*
+	when using insert: copy constructor is called - very expensive!
+	1. Student(123, "sumit") is created as a temporary object.
+	2. 1st copy: {1, Student(123, "sumit")} creates a std::pair<in, Student>, means Student obj. is copied into the pair
+	3. 2nd copy: map needs to store this pair inside its internal structure, so it copies the Student object again.
+
+	with emplace():
+	Student(239, "priyanshu") -> is constructed then copied in map so 1 copy!
+
+	with std::piecewise_construct
+	- constructs the student object directly inside map's memory
+	*/
+	/*
+//	studMap[6] = Student(10, "aman"); // this doesnt work - because operator[] requires a default-constructible value type
+
+	// making default constructor inside Student, then trying again
+	studMap[6] = Student(10, "aman");
+	/ in the above case(using [] operator) : first temporary object is created then copy assignment is called
+
+	*/
+
+	//std::map<int, int>mii;
+	//mii.insert(std::pair<int,int>(12, 3)); // type is deduced automatically
+	//mii.insert(std::make_pair(14,8)); // explicitly defined types
+	//for (const auto& it : mii) {
+	//	std::cout << it.first << " " << it.second << std::endl;
+	//}
+
+	// reference binding - C++ 17 concept
+	// Structured Binding (introduced in C++17) allows unpacking objects into multiple named variables in a single statement.
+	
+	/*for (const auto& [key, value] : mii) {
+		std::cout << key << " -> " << value << "\n";
+	}*/
+	// or
+	std::map<int, std::map<int, int>>mimii = { {2,{{4,5}, {5,6}}},{3,{{1,2},{6,7}}},{1,{{3,4},{5,9}}},{5,{{5,8},{1,1}}} };
+	for (const auto& [outerKey, innerMap] : mimii) {
+		std::cout << "for key: " << outerKey << "\n";
+		for (const auto& [innerKey, innerVal] : innerMap) {
+			std::cout << innerKey << " -> " << innerVal << std::endl;
+		}
+	}
+
+	// imp: insert_or_assign() - C++17 feature
+	std::map<int, std::string>mis;
+	mis[5] = "crazy"; // default-constructs empty string first, then assigns - 2 steps!
+	mis.insert_or_assign(6, "batman"); // directly constructs the pair - no default creation - better performance!
+
+	/*
+	emplace vs try_emplace
+		emplace:
+	- Constructs the value immediately (memory is allocated).
+	- Then checks if key exists.
+	- If key exists → value is discarded, memory is freed.
+
+		try_emplace:
+	- First checks if key exists.
+	- If not, then value is constructed in-place and inserted.
+	- Avoids unnecessary construction + allocation.
+	*/
+
+	mis.try_emplace(6, "batman");
+	mis.try_emplace(7, "dolphin");
+
+	for (auto& [key, val] : mis) {
+		std::cout << key << " " << val << "\n";
+	}
+
+	return 0;
+}
